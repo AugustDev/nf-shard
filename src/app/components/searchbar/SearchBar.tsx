0e35bdbc45a8c9492d3325168a092fdcf61987b6
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-09-02 19:24:01 +0100

    search

--- src/app/components/searchbar/SearchBar.tsx
+++ src/app/components/searchbar/SearchBar.tsx
@@ -1,8 +1,12 @@
 type SearchProps = {
   onChange: (value: string) => void;
+  onSubmit: () => void;
 };
 
-export const SearchBar: React.FC<SearchProps> = ({ onChange }: SearchProps) => {
+export const SearchBar: React.FC<SearchProps> = ({
+  onChange,
+  onSubmit,
+}: SearchProps) => {
   return (
     <>
       <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
@@ -32,10 +36,12 @@ export const SearchBar: React.FC<SearchProps> = ({ onChange }: SearchProps) => {
           className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search runs, workflow id, tags etc"
           onChange={(e) => onChange(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? onSubmit() : undefined)}
         />
         <button
           type="submit"
           className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={onSubmit}
         >
           Search
         </button>
--- src/app/runs/components/Main/Main.tsx
+++ src/app/runs/components/Main/Main.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { clsx } from "clsx";
 import { SearchBar } from "@/app/components";
 import { Workflow } from "@prisma/client";
 import { RunsTable } from "../RunsTable";
+import { SearchResponse } from "@/app/api/search/types";
+import styles from "./Main.module.scss";
 
 type TMainProps = {
   runs: Workflow[];
@@ -13,12 +16,31 @@ export const Main = (props: TMainProps) => {
   const [search, setSearch] = useState("");
   const [workflows, setWorkflows] = useState<Workflow[]>(props.runs);
 
-  const executeSearch = () => {};
+  const executeSearch = async () => {
+    if (search.length < 3) {
+      setWorkflows(props.runs);
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3000/api/search`, {
+      body: JSON.stringify({ term: search }),
+      method: "POST",
+      cache: "no-store",
+    });
+
+    const results: SearchResponse = await response.json();
+    setWorkflows(results.workflows);
+  };
 
   return (
     <>
-      <SearchBar onChange={setSearch} />
-      <RunsTable runs={workflows} className="mt-8" />
+      <SearchBar onChange={setSearch} onSubmit={executeSearch} />
+      {workflows.length > 0 && (
+        <RunsTable
+          runs={workflows}
+          className={clsx(styles.fadeInBottom, "mt-8")}
+        />
+      )}
     </>
   );
 };
