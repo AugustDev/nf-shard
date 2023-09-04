74b4c5473f9643bc8eac01722d9224daabe299cf
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-09-04 21:21:12 +0100

    ver

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
-};
+	distDir: "build",
+	experimental: {
+		appDir: true,
+	},
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
