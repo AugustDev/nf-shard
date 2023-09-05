8fd6322daeb13559076d10826c2cdeab4e29ed26
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-09-05 18:12:43 +0100

    ver

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
 	experimental: {
 		appDir: true,
 	},
+	// https://github.com/aws-amplify/amplify-hosting/issues/1987
+	env: {
+		POSTGRES_URI: process.env.POSTGRES_URI,
+		NEXT_PUBLIC_WATCH_BASE_URI: process.env.NEXT_PUBLIC_WATCH_BASE_URI,
+	},
 }
 
 module.exports = nextConfig
