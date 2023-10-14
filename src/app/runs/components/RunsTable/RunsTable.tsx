c3cd0cb629bc733b6f7330c42c0e8446671fa31d
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-10-14 10:41:58 +0100

    fix: runs table layout fixes

--- src/app/runs/components/RunsTable/RunsTable.tsx
+++ src/app/runs/components/RunsTable/RunsTable.tsx
@@ -8,6 +8,7 @@ import { Workflow } from "@prisma/client"
 import { clsx } from "clsx"
 import { OptionsDropdown } from "../OptionsDropdown"
 import Link from "next/link"
+import React from "react"
 
 type RunsTableProps = {
 	runs: Workflow[]
@@ -17,42 +18,36 @@ type RunsTableProps = {
 
 export const RunsTable: React.FC<RunsTableProps> = ({ runs, className, onDeleteClick }: RunsTableProps) => {
 	return (
-		<div className={clsx(className, "overflow-x-auto h-full mx-4 md:mx-0")}>
-			<div className="rounded-md text-left bg-white min-w-max">
+		<div className={clsx(className, "overflow-x-auto h-full mx-4 md:mx-0 bg-white")}>
+			<div className="rounded-md text-left bg-white">
 				{runs.map((run) => (
 					<Link href={`/runs/${run.id}`} key={run.id}>
-						<div
-							key={run.id}
-							className="grid hover:bg-gray-50 cursor-pointer align-middle sm:px-6 lg:px-8"
-							style={{ gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr auto" }}
-						>
-							<div className="whitespace-nowrap px-6 py-5 text-sm text-gray-500 flex-auto">
+						<div className="grid grid-cols-[minmax(auto,1fr),minmax(auto,1fr),minmax(auto,1fr),minmax(auto,1fr),minmax(auto,1fr),min-content] gap-4 hover:bg-gray-50 cursor-pointer align-middle sm:px-6 lg:px-8">
+							<div className="px-6 py-5 text-sm text-gray-500 min-w-[300px]">
 								<div className="text-gray-900">{run.manifest.description}</div>
 							</div>
-							<div className="whitespace-nowrap py-5 px-3 text-sm sm:pl-0 flex-auto">
-								<div className="flex items-center">
-									<div className="ml-4">
-										<div className="font-medium text-gray-900">{run.runName}</div>
-										<div className="mt-1 text-gray-500">{run.projectName}</div>
-									</div>
+							<div className="py-5 px-3 text-sm flex items-center min-w-[200px]">
+								<div className="ml-4">
+									<div className="font-medium text-gray-900">{run.runName}</div>
+									<div className="mt-1 text-gray-500">{run.projectName}</div>
 								</div>
 							</div>
-							<div className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 text-right flex-auto">
+							<div className="px-3 py-5 text-sm text-gray-500 text-right min-w-[200px]">
 								<div className="font-medium text-gray-900">{fullDateTime(run.start)}</div>
-								<div className="text-gray-500 mt-1">
+								<div className="mt-1">
 									{run.complete && <div>{formatDifference(run.start, run.complete)}</div>}
 									{!run.complete && <TimerDisplayDynamic startedAt={run.start} />}
 								</div>
 							</div>
-							<div className="whitespace-nowrap px-3 py-5 text-sm grid-column-span-2 text-right flex justify-center items-center flex-auto w-1/8">
+							<div className="px-3 py-5 text-sm flex justify-center items-center">
 								{run.tags.map((tag) => (
 									<Tag key={tag} name={tag} />
 								))}
 							</div>
-							<div className="whitespace-nowrap py-5 text-sm text-gray-500 flex justify-center items-center">
+							<div className="py-5 text-sm text-gray-500 flex justify-center items-center min-w-[150px]">
 								<WorkflowStatusTag status={workflowStatus(run)} />
 							</div>
-							<div className="whitespace-nowrap py-5 text-sm text-gray-500 flex justify-end items-center">
+							<div className="py-5 text-sm text-gray-500 flex justify-end items-center">
 								<OptionsDropdown deleteWorkflow={() => onDeleteClick(run.id)} />
 							</div>
 						</div>
