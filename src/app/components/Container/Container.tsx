7faadc39ce5867d5f37e0c46929846c82b4eb2af
Augustinas Malinauskas <augustinasmal@gmail.com>
2024-07-11 08:59:44 +0100

    fix: fonts

--- src/app/components/Container/Container.tsx
+++ src/app/components/Container/Container.tsx
@@ -2,15 +2,17 @@ import { clsx } from "clsx"
 
 type ContainerProps = {
 	children: React.ReactNode
+	headerChildren?: React.ReactNode
 	sectionName: string
 	className?: string
 }
 
-export const Container = ({ children, className, sectionName }: ContainerProps) => {
+export const Container = ({ children, className, sectionName, headerChildren }: ContainerProps) => {
 	return (
 		<div className={clsx(className, "bg-white shadow-sm rounded-s mx-4 md:mx-0")}>
-			<header className="px-5 py-4 border-b border-gray-100">
+			<header className="flex items-center gap-x-4 px-5 py-4 border-b border-gray-100">
 				<h2 className="font-semibold text-gray-800">{sectionName}</h2>
+				{headerChildren}
 			</header>
 			<div className="px-6 py-6">{children}</div>
 		</div>
